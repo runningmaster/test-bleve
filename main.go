a4b6c7b9abe85478bd9f4c71d8277df9069c79de
Dmitriy Kovalenko <runningmaster@gmail.com>
2017-09-11 00:30:37 +0300

    add baseDoc

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"net/url"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"sync"
 	"syscall"
@@ -23,11 +24,12 @@ import (
 	"golang.org/x/text/language"
 )
 
-// $ curl -i -X POST -T /path/to/data.csv http://localhost:8080/test/upload-suggestion
+// $ curl -i -X POST -T /path/to/data.csv http://localhost:8080/test/upload-sugg
 // $ curl -i -d '{"name": "foo bar"}' http://localhost:8080/test/select-suggestion
 
 var indexDB = &index{
-	store: make(map[string]bleve.Index, 4),
+	store: make(map[string]bleve.Index, 10),
+	vault: make(map[string]*sync.Map, 10),
 }
 
 func main() {
@@ -41,8 +43,10 @@ func main() {
 }
 
 func setupHandler(m *http.ServeMux) http.Handler {
-	m.HandleFunc("/test/upload-suggestion", uploadSuggestion)
+	m.HandleFunc("/test/upload-sugg", uploadSugg)
 	m.HandleFunc("/test/select-suggestion", selectSuggestion)
+	m.HandleFunc("/test/select-sugg", selectSugg)
+	m.HandleFunc("/test/select-name", selectName)
 	return m
 }
 
@@ -77,7 +81,16 @@ func listenForShutdown(s *http.Server, ch <-chan os.Signal) {
 	}
 }
 
-func uploadSuggestion(w http.ResponseWriter, r *http.Request) {
+type baseDoc struct {
+	ID   int    `json:"id,omitempty"`
+	Kind string `json:"kind,omitempty"`
+	Code string `json:"code,omitempty"`
+	Name string `json:"name,omitempty"`
+	Info int    `json:"info,omitempty"`
+	Sale int    `json:"sale,omitempty"`
+}
+
+func uploadSugg(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 		return
@@ -96,52 +109,62 @@ func uploadSuggestion(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	vltATCru := &sync.Map{}
 	idxATCru, err := bleve.NewMemOnly(bleve.NewIndexMapping())
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	vltINFru := &sync.Map{}
 	idxINFru, err := bleve.NewMemOnly(bleve.NewIndexMapping())
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	vltINNru := &sync.Map{}
 	idxINNru, err := bleve.NewMemOnly(bleve.NewIndexMapping())
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	vltACTru := &sync.Map{}
 	idxACTru, err := bleve.NewMemOnly(bleve.NewIndexMapping())
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	vltORGru := &sync.Map{}
 	idxORGru, err := bleve.NewMemOnly(bleve.NewIndexMapping())
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	vltATCua := &sync.Map{}
 	idxATCua, err := bleve.NewMemOnly(bleve.NewIndexMapping())
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	vltINFua := &sync.Map{}
 	idxINFua, err := bleve.NewMemOnly(bleve.NewIndexMapping())
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	vltINNua := &sync.Map{}
 	idxINNua, err := bleve.NewMemOnly(bleve.NewIndexMapping())
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	vltACTua := &sync.Map{}
 	idxACTua, err := bleve.NewMemOnly(bleve.NewIndexMapping())
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	vltORGua := &sync.Map{}
 	idxORGua, err := bleve.NewMemOnly(bleve.NewIndexMapping())
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -149,33 +172,80 @@ func uploadSuggestion(w http.ResponseWriter, r *http.Request) {
 	}
 
 	for i := range rec {
-		if len(rec[i]) < 4 {
-			err = fmt.Errorf("invalid csv: got %d, want %d", len(rec[i]), 4)
+		if len(rec[i]) < 6 {
+			err = fmt.Errorf("invalid csv: got %d, want %d", len(rec[i]), 6)
 		}
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		switch rec[i][0] {
-		case "atc":
-			// fucking workaround
-			key := rec[i][1] + "|" + strings.TrimSpace(strings.Replace(strings.Split(rec[i][2], "|")[0], " ", "", -1))
-			idxATCru.Index(key, strings.TrimSpace(strings.Replace(rec[i][2], "|", " ", 1)))
-			idxATCua.Index(key, strings.TrimSpace(strings.Replace(rec[i][3], "|", " ", 1)))
-		case "info":
-			idxINFru.Index(rec[i][1], strings.TrimSpace(rec[i][2]))
-			idxINFua.Index(rec[i][1], strings.TrimSpace(rec[i][3]))
-		case "inn":
-			idxINNru.Index(rec[i][1], strings.TrimSpace(rec[i][2]))
-			idxINNua.Index(rec[i][1], strings.TrimSpace(rec[i][3]))
-		case "act":
-			idxACTru.Index(rec[i][1], strings.TrimSpace(rec[i][2]))
-			idxACTua.Index(rec[i][1], strings.TrimSpace(rec[i][3]))
-		case "org":
-			idxORGru.Index(rec[i][1], strings.TrimSpace(rec[i][2]))
-			idxORGua.Index(rec[i][1], strings.TrimSpace(rec[i][3]))
+
+		docRU := &baseDoc{}
+		docRU.ID, _ = strconv.Atoi(rec[i][1])
+		docRU.Kind = rec[i][0]
+		docRU.Name = rec[i][2]
+		docRU.Info, _ = strconv.Atoi(rec[i][4])
+		docRU.Sale, _ = strconv.Atoi(rec[i][5])
+
+		docUA := &baseDoc{}
+		docUA.ID, _ = strconv.Atoi(rec[i][1])
+		docUA.Kind = rec[i][0]
+		docUA.Name = rec[i][3]
+		docUA.Info, _ = strconv.Atoi(rec[i][4])
+		docUA.Sale, _ = strconv.Atoi(rec[i][5])
+
+		key := rec[i][1]
+		// fucking workaround
+		if docRU.Kind == "atc" {
+			docRU.Code = strings.TrimSpace(strings.Split(docRU.Name, "|")[0])
+			//docRU.Name = strings.TrimSpace(strings.Replace(docRU.Name, "|", " ", 1))
+			key = key + "|" + strings.Replace(docRU.Code, " ", "", -1)
+		}
+		if docUA.Kind == "atc" {
+			docUA.Code = strings.TrimSpace(strings.Split(docUA.Name, "|")[0])
+			//docUA.Name = strings.TrimSpace(strings.Replace(docUA.Name, "|", " ", 1))
+			key = key + "|" + strings.Replace(docUA.Code, " ", "", -1)
 		}
 
+		if docRU.Kind == "info" {
+			docRU.Kind = "inf"
+		}
+		if docUA.Kind == "info" {
+			docUA.Kind = "inf"
+		}
+
+		switch docRU.Kind {
+		case "atc":
+			idxATCru.Index(key, docRU.Name)
+			idxATCua.Index(key, docUA.Name)
+
+			vltATCru.Store(key, docRU)
+			vltATCua.Store(key, docUA)
+		case "inf":
+			idxINFru.Index(key, docRU.Name)
+			idxINFua.Index(key, docUA.Name)
+
+			vltINFru.Store(key, docRU)
+			vltINFua.Store(key, docUA)
+		case "inn":
+			idxINNru.Index(key, docRU.Name)
+			idxINNua.Index(key, docUA.Name)
+
+			vltINNru.Store(key, docRU)
+			vltINNua.Store(key, docUA)
+		case "act":
+			idxACTru.Index(key, docRU.Name)
+			idxACTua.Index(key, docUA.Name)
+
+			vltACTru.Store(key, docRU)
+			vltACTua.Store(key, docUA)
+		case "org":
+			idxORGru.Index(key, docRU.Name)
+			idxORGua.Index(key, docUA.Name)
+
+			vltORGru.Store(key, docRU)
+			vltORGua.Store(key, docUA)
+		}
 	}
 
 	indexDB.setIndex("atc-ru", idxATCru)
@@ -189,6 +259,18 @@ func uploadSuggestion(w http.ResponseWriter, r *http.Request) {
 	indexDB.setIndex("inn-ua", idxINNua)
 	indexDB.setIndex("act-ua", idxACTua)
 	indexDB.setIndex("org-ua", idxORGua)
+
+	indexDB.setVault("atc-ru", vltATCru)
+	indexDB.setVault("inf-ru", vltINFru)
+	indexDB.setVault("inn-ru", vltINNru)
+	indexDB.setVault("act-ru", vltACTru)
+	indexDB.setVault("org-ru", vltORGru)
+
+	indexDB.setVault("atc-ua", vltATCua)
+	indexDB.setVault("inf-ua", vltINFua)
+	indexDB.setVault("inn-ua", vltINNua)
+	indexDB.setVault("act-ua", vltACTua)
+	indexDB.setVault("org-ua", vltORGua)
 
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintln(w, len(rec))
@@ -349,6 +431,7 @@ func selectSuggestion(w http.ResponseWriter, r *http.Request) {
 			keys[i] = strings.Split(keys[i], "|")[0]
 		}
 		s.Keys = append(s.Keys, keys...)
+		s.Name = strings.TrimSpace(strings.Replace(s.Name, "|", " ", 1))
 		res.SuggATC = append(res.SuggATC, s)
 	}
 	for i := range sINF {
@@ -387,14 +470,234 @@ func selectSuggestion(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, string(b))
 }
 
+func selectSugg(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	b, err := ioutil.ReadAll(r.Body)
+	defer func() { _ = r.Body.Close() }()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	v := struct {
+		Name string `json:"name"`
+	}{}
+
+	err = json.Unmarshal(b, &v)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	n := len([]rune(v.Name))
+	if n <= 2 {
+		err = fmt.Errorf("too few characters: %d", n)
+	} else if n > 128 {
+		err = fmt.Errorf("too many characters: %d", n)
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	idxATC := "atc-ru"
+	idxINF := "inf-ru"
+	idxINN := "inn-ru"
+	idxACT := "act-ru"
+	idxORG := "org-ru"
+	if langUA(r.Header) {
+		idxATC = "atc-ua"
+		idxINF = "inf-ua"
+		idxINN = "inn-ua"
+		idxACT = "act-ua"
+		idxORG = "org-ua"
+	}
+
+	mATC, err := findByName(idxATC, v.Name)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	mINF, err := findByName(idxINF, v.Name)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	mINN, err := findByName(idxINN, v.Name)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	mACT, err := findByName(idxACT, v.Name)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	mORG, err := findByName(idxORG, v.Name)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	convName := convString(v.Name, "en", "ru")
+	if langUA(r.Header) {
+		convName = convString(v.Name, "en", "uk")
+	}
+	if len(mATC) == 0 {
+		mATC, err = findByName(idxATC, convName)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	if len(mINF) == 0 {
+		mINF, err = findByName(idxINF, convName)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	if len(mINN) == 0 {
+		mINN, err = findByName(idxINN, convName)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	if len(mACT) == 0 {
+		mACT, err = findByName(idxACT, convName)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	if len(mORG) == 0 {
+		mORG, err = findByName(idxORG, convName)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	mAll := make(map[string]struct{}, len(mATC)+len(mINF)+len(mINN)+len(mACT)+len(mORG))
+	for k := range mATC {
+		mAll[strings.ToUpper(strings.TrimSpace(strings.Replace(strings.Split(k, "|")[1], "|", " ", 1)))] = struct{}{}
+	}
+	for k := range mINF {
+		mAll[strings.ToUpper(k)] = struct{}{}
+	}
+	for k := range mINN {
+		mAll[strings.ToUpper(k)] = struct{}{}
+	}
+	for k := range mACT {
+		mAll[strings.ToUpper(k)] = struct{}{}
+	}
+	for k := range mORG {
+		mAll[strings.ToUpper(k)] = struct{}{}
+	}
+	sAll := make([]string, 0, len(mAll))
+	for k := range mAll {
+		sAll = append(sAll, k)
+	}
+
+	// Sorting
+	c := collate.New(language.Russian)
+	if langUA(r.Header) {
+		c = collate.New(language.Ukrainian)
+	}
+	c.SortStrings(sAll)
+
+	res := result{Find: v.Name}
+	for i := range sAll {
+		if strings.HasPrefix(strings.ToLower(sAll[i]), strings.ToLower(convName)) {
+			res.Sugg = append(res.Sugg, sAll[i])
+		}
+	}
+	for i := range sAll {
+		if !strings.HasPrefix(strings.ToLower(sAll[i]), strings.ToLower(convName)) {
+			res.Sugg = append(res.Sugg, sAll[i])
+		}
+	}
+
+	b, err = json.MarshalIndent(res, "", "\t")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, string(b))
+}
+
+func selectName(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	b, err := ioutil.ReadAll(r.Body)
+	defer func() { _ = r.Body.Close() }()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	v := struct {
+		Name string `json:"name"`
+	}{}
+
+	err = json.Unmarshal(b, &v)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	n := len([]rune(v.Name))
+	if n <= 2 {
+		err = fmt.Errorf("too few characters: %d", n)
+	} else if n > 128 {
+		err = fmt.Errorf("too many characters: %d", n)
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//idx := "ru"
+	//if langUA(r.Header) {
+	//	idx = "ua"
+	//}
+
+	//convName := convString(v.Name, "en", "ru")
+	//if langUA(r.Header) {
+	//	convName = convString(v.Name, "en", "uk")
+	//}
+
+	b, err = json.MarshalIndent([]byte("{\"name\":\"bar\"}"), "", "\t")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, string(b))
+}
+
 type result struct {
-	Find     string `json:"find,omitempty"`
-	SuggATC  []sugg `json:"sugg_atc,omitempty"`
-	SuggINF1 []sugg `json:"sugg_inf1,omitempty"`
-	SuggINF2 []sugg `json:"sugg_inf2,omitempty"`
-	SuggINN  []sugg `json:"sugg_inn,omitempty"`
-	SuggACT  []sugg `json:"sugg_act,omitempty"`
-	SuggORG  []sugg `json:"sugg_org,omitempty"`
+	Find     string   `json:"find,omitempty"`
+	Sugg     []string `json:"sugg,omitempty"`
+	SuggATC  []sugg   `json:"sugg_atc,omitempty"`
+	SuggINF1 []sugg   `json:"sugg_inf1,omitempty"`
+	SuggINF2 []sugg   `json:"sugg_inf2,omitempty"`
+	SuggINN  []sugg   `json:"sugg_inn,omitempty"`
+	SuggACT  []sugg   `json:"sugg_act,omitempty"`
+	SuggORG  []sugg   `json:"sugg_org,omitempty"`
 }
 
 type sugg struct {
@@ -441,14 +744,10 @@ func findByName(key, name string) (map[string][]string, error) {
 	return out, nil
 }
 
-//type indexer interface {
-//	getIndex(string) (bleve.Index, error)
-//	setIndex(string, bleve.Index) error
-//}
-
 type index struct {
 	sync.RWMutex
 	store map[string]bleve.Index
+	vault map[string]*sync.Map
 }
 
 func (i *index) getIndex(key string) (bleve.Index, error) {
@@ -466,6 +765,21 @@ func (i *index) getIndex(key string) (bleve.Index, error) {
 	return nil, fmt.Errorf("index not found")
 }
 
+func (i *index) getVault(key string) (*sync.Map, error) {
+	i.RLock()
+	defer i.RUnlock()
+
+	if i.vault == nil {
+		return nil, fmt.Errorf("index vault is nil")
+	}
+
+	if vlt, ok := i.vault[key]; ok {
+		return vlt, nil
+	}
+
+	return nil, fmt.Errorf("vault not found")
+}
+
 func (i *index) setIndex(key string, idx bleve.Index) error {
 	i.Lock()
 	defer i.Unlock()
@@ -479,6 +793,23 @@ func (i *index) setIndex(key string, idx bleve.Index) error {
 	}
 
 	i.store[key] = idx
+
+	return nil
+}
+
+func (i *index) setVault(key string, vlt *sync.Map) error {
+	i.Lock()
+	defer i.Unlock()
+
+	if i.vault == nil {
+		return fmt.Errorf("index vault is nil")
+	}
+
+	if _, ok := i.vault[key]; ok {
+		delete(i.vault, key)
+	}
+
+	i.vault[key] = vlt
 
 	return nil
 }
